20037edd5bbc4a2db00d8a33d25fa03f7f8bc22d
Ronnie-8055 <ronnie48054805@gmail.com>
2025-06-06 23:22:04 +0530

    quick sort.c

--- quick sort.c
+++ quick sort.c
@@ -42,23 +42,23 @@ int pivot_index = partition(arr, low, high);quickSort(arr, low, pivot_index - 1)
 quickSort(arr, pivot_index + 1, high);
 }
 }
-int partition(int arr[], int low, int high)
-{
-int pivot = arr[high];
-int i = low - 1;
-for (int j = low; j < high; j++)
-{
-if (arr[j] < pivot)
-{
-i++;
-int temp = arr[i];
-arr[i] = arr[j];
-arr[j] = temp;
-}
-int temp = arr[i + 1];
-arr[i + 1] = arr[high];
-arr[high] = temp;
-return i + 1;
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
 }
 void printArray(int arr[], int size)
 {
@@ -67,5 +67,4 @@ for (int i = 0; i < size; i++)
 printf("%d ", arr[i]);
 }
 printf("\n");
-}
 }
